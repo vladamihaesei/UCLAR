195897cfde903f6d9da9e8bccc8b2250e40ec47a
vladamihaesei <vlad.amihaesei95@gmail.com>
2022-07-11 21:09:57 +0300

    ##modis download

--- R/modis_download/modis_download.R
+++ R/modis_download/modis_download.R
@@ -12,18 +12,22 @@ limite <- read_sf("Limite/Limite.shp")
 for (i in 1:length(limite)){
   
   limite1 <- limite[i,]
+  nume <- limite1$Name
   bb <- st_bbox(limite1)
   
+  path <- paste0('/Volumes/Z_vld/sfica_proiect_NE/',nume)
+  if (!dir.exists(path)) dir.create(path, recursive = T)
+  
   MODIStsp(gui             = FALSE,
-           out_folder      = '/Volumes/Z_vld/sfica_proiect_NE/',
-           out_folder_mod  = '/Volumes/Z_vld/sfica_proiect_NE/',
+           out_folder      = path,
+           out_folder_mod  = path,
            selprod         = 'Surf_Temp_Daily_1Km (M*D11A1)',
-           bandsel         = c('Clear_day_cov','LST_Day_1km','LST_Night_1km'), 
+           bandsel         = c('QC_Day','LST_Day_1km','LST_Night_1km',"QC_night"), 
            sensor          = 'Both',
            user            = 'vlad.amihaesei95@gmail.com' , # your username for NASA http server
            password        = 'Geografie10',  # your password for NASA http server
            start_date      = '2004.06.01', 
-           end_date        = '2004.06.04', 
+           end_date        = '2004.06.03', 
            verbose         = TRUE,
            bbox            = c(bb[1], bb[2], bb[3], bb[4]), #bbox of Latam
            spatmeth        = 'bbox',
@@ -41,6 +45,7 @@ for (i in 1:length(limite)){
 
 ### verificare fisiere 
 
-files <- list.files("/Volumes/Z_vld/sfica_proiect_NE/Surf_Temp_Daily_1Km_v6/Clear_day_cov", pattern = ".tif",full.names = T)
+files <- list.files("/Volumes/Z_vld/sfica_proiect_NE/Surf_Temp_Daily_1Km_v6/LST_Day_1km", pattern = ".tif",full.names = T)
 
 vf <- terra::rast(files)
+plot(vf)
