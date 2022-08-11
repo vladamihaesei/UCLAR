22c6fa154f4f47252ec1bbb6fb459591fb79bad3
vladamihaesei <vlad.amihaesei95@gmail.com>
2022-08-11 09:50:17 +0300

    ### modis

--- R/modis_download/modis_download.R
+++ R/modis_download/modis_download.R
@@ -14,19 +14,21 @@ for (i in 1:length(limite)){
   nume <- limite1$Drawings
   bb <- st_bbox(limite1)
   
-  path <- paste0('/Volumes/Z_vld/sfica_proiect_NE/',nume) 
+  path <- paste0('tmp/',nume) 
   if (!dir.exists(path)) dir.create(path, recursive = T)
   
   MODIStsp(gui             = FALSE,
            out_folder      = path,
            out_folder_mod  = path,
            selprod         = 'Surf_Temp_Daily_1Km (M*D11A1)',
-           bandsel         = c('QC_Day','LST_Day_1km','LST_Night_1km',"QC_night"), 
+           bandsel         = c('QC_Day','LST_Day_1km','LST_Night_1km'), 
            sensor          = 'Both',
            user            = 'vlad.amihaesei95@gmail.com' , # your username for NASA http server
            password        = 'Geografie10',  # your password for NASA http server
            start_date      = '2004.06.01', 
            end_date        = '2004.06.03', 
+           download_server = 'http',
+           #downloader      = 'aria2',
            verbose         = TRUE,
            bbox            = c(bb[1], bb[2], bb[3], bb[4]), #bbox of Latam
            spatmeth        = 'bbox',
@@ -34,12 +36,18 @@ for (i in 1:length(limite)){
            compress        = 'LZW',
            out_projsel     = 'User Defined',
            output_proj     = '+proj=longlat +datum=WGS84 +no_defs',
-           delete_hdf      = TRUE,
+           delete_hdf      = T,
            parallel        = TRUE
   )
   
 }
+library(httr)
+httr::GET("http://cran.r-project.org/Rlogo.jpg", config = httr::config(connecttimeout = 90))
+GET("http://httpbin.org/delay/1", timeout(20))
 
+GET("http://had.co.nz")
+
+system("ping -c5 httpbin.org")
 
 ### verificare fisiere 
 files <- list.files("/Volumes/Z_vld/sfica_proiect_NE/Surf_Temp_Daily_1Km_v6/LST_Day_1km", pattern = ".tif",full.names = T)
--- R/modis_gapfilling/count_na.R
+++ R/modis_gapfilling/count_na.R
@@ -2,25 +2,44 @@ library(dplyr)
 library(terra)
 library(ggplot2)
 ### rasterele de analizat 
-files <- list.files(path = "tmp/Barlad/Surf_Temp_Daily_1Km_v6/LST_Day_1km", pattern = "*.tif", recursive = T, full.names = T )
-files.sub <- grep("MOD11A1",files, value =T)## Aqua
-files.sub1 <- grep("MYD11A1",files, value =T)##Terra 
 
-dats <- as.Date(paste0(do.call(rbind,strsplit(files.sub,"/|_"))[,15],"-",do.call(rbind,strsplit(files.sub,"/|_|.tif"))[,16]), format("%Y-%j"))
-dats1 <- as.Date(paste0(do.call(rbind,strsplit(files.sub1,"/|_"))[,15],"-",do.call(rbind,strsplit(files.sub1,"/|_|.tif"))[,16]), format("%Y-%j"))
+all <- list.files(path = "tmp/", pattern = "*.tif", recursive = T, full.names = T )
 
-tab <- NULL
-
-for(i in 1:length(files.sub)){
+nume <-  unique(do.call(rbind,strsplit(all,"/"))[,3])
+dn <- c("Day","Night")
+for(n in 1:length(nume)){
   
-  print(dats[i])
-  rss <- terra::rast(files.sub[i])
-  frec <- (sum(is.na(values(rss)))*100)/length(values(rss))
-  dat <- dats[i]
-  t <- data.frame(timp = dat, frecventa = frec, tip = "MOD11A1")
-  tab <- rbind(tab,t)
+  for(d in 1:length(dn)){
+    
+  
+    files <- list.files(path = paste0("tmp/",nume[n],"/Surf_Temp_Daily_1Km_v6/LST_",dn[d],"_1km"), pattern = "*.tif", recursive = T, full.names = T )
+    files.sub <- grep("MOD11A1",files, value =T)## Aqua
+    files.sub1 <- grep("MYD11A1",files, value =T)##Terra 
+    
+    dats <- as.Date(paste0(do.call(rbind,strsplit(files.sub,"/|_"))[,15],"-",do.call(rbind,strsplit(files.sub,"/|_|.tif"))[,16]), format("%Y-%j"))
+    dats1 <- as.Date(paste0(do.call(rbind,strsplit(files.sub1,"/|_"))[,15],"-",do.call(rbind,strsplit(files.sub1,"/|_|.tif"))[,16]), format("%Y-%j"))
+    
+    tab <- NULL
+    
+    for(i in 1:length(files.sub)){
+      
+      print(dats[i])
+      rss <- terra::rast(files.sub[i])
+      frec <- (sum(is.na(values(rss)))*100)/length(values(rss))
+      dat <- dats[i]
+      t <- data.frame(timp = dat, frecventa = frec, tip = "MOD11A1")
+      tab <- rbind(tab,t)
+      
+    }
+    
+    
+  }
 
+  
+  
 }
+
+
 
 tab1 <- NULL
 
@@ -39,17 +58,22 @@ tot <- rbind(tab,tab1)
 
 tot <- tot%>%mutate(year  = format(timp,"%Y"),
                     month = format(timp,"%m"))
-write.csv(tot,"tabs/count_na_Barlad.csv", row.names =F)
+write.csv(tot,"tabs/count_na_Botosani.csv", row.names =F)
 
+tot.f <- tot%>%filter(!frecventa %in%c(0,100))
 
-g  <- ggplot(tot, aes(x= frecventa))+
+g  <- ggplot(tot.f, aes(x= frecventa))+
   geom_histogram(color="darkblue", fill="lightblue")+facet_wrap(~tip)+theme_bw()+
   scale_x_continuous(expand = c(0, 0))+scale_y_continuous(expand = c(0, 0))
-
-png("png/histrogam_countNA_Barlad.png", width = 1500, height = 1300, res = 200)
+g
+png("png/histrogam_countNA_Baotosani.png", width = 1500, height = 1300, res = 200)
 g
 dev.off()
 
-g1 <- ggplot(tot, aes(x= frecventa))+
-  geom_histogram(color="darkblue", fill="lightblue")+facet_wrap(~tip)
+g1 <- ggplot(tot.f, aes(x= frecventa))+
+  geom_histogram(color="darkblue", fill="lightblue")+facet_grid(month~tip)+theme_bw()+
+scale_x_continuous(expand = c(0, 0))+scale_y_continuous(expand = c(0, 0))
 
+png("png/histrogam_monthly_countNA_Barlad.png", width = 1800, height = 1400, res = 230)
+g1
+dev.off()
